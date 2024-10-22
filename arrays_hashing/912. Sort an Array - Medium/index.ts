0590ac83c79de5e1bebc5ad1247a475989295429
Mirjahon Ulmasov <mirjahonulmasov@gmail.com>
2024-10-22 17:58:36 +0500

    solved leetcode

--- arrays_hashing/912. Sort an Array - Medium/index.ts
+++ arrays_hashing/912. Sort an Array - Medium/index.ts
@@ -18,6 +18,40 @@ Constraints:
 -5 * 104 <= nums[i] <= 5 * 104
 */
 
+// Using Merge Sort
 function sortArray(nums: number[]): number[] {
-    return []
+    return mergeSort(nums, 0, nums.length - 1)
 }
+
+function mergeSort(arr: number[], left: number, right: number) {
+    if (left == right) return arr
+
+    const mid = Math.floor((left + right) / 2)
+    mergeSort(arr, left, mid)
+    mergeSort(arr, mid + 1, right)
+    merge(arr, left, mid, right)
+    return arr
+}
+
+function merge(arr: number[], L: number, M: number, R: number) {
+    const leftArr = arr.slice(L, M + 1)
+    const rightArr = arr.slice(M + 1, R + 1)
+    let i = L, j = 0, k = 0
+
+    while (j < leftArr.length && k < rightArr.length) {
+        if (leftArr[j] <= rightArr[k]) {
+            arr[i++] = rightArr[k++]
+        } else {
+            arr[i++] = leftArr[j++]
+        }
+    }
+    while (j < leftArr.length) {
+        arr[i++] = leftArr[j++]
+    }
+    while (k < rightArr.length) {
+        arr[i++] = rightArr[k++]
+    }
+    return arr
+}
+
+console.log(sortArray([5,2,3,1]));
